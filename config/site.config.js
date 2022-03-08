43e1793062d28601b628be50bcabdd1aa269dc51
hhhhhai666 <101188740+hhhhhai666@users.noreply.github.com>
2022-03-08 23:33:15 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/1a备份软件/lsposed',],
+  protectedRoutes: ['/huahai分享/1a备份软件/lsposed',],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '1678895137@qq.com',
